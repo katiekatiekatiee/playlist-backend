0d7f3ac5cac42d8ef11aedb6aad9697e6520b1c2
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-09-27 21:24:20 -0400

    used resource generator to create tables

---
+++ db/migrate/20210928012338_create_songs.rb
@@ -0,0 +1,11 @@
+class CreateSongs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :songs do |t|
+      t.string :title
+      t.string :artist
+      t.belongs_to :playlist, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
