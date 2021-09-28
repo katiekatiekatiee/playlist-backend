0ca1d8e35c74b2ebaa200c8a39a5d0840d3f78d6
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-09-28 19:02:15 -0400

    added strong params

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,2 +1,29 @@
 class PlaylistsController < ApplicationController
+    def index
+        playlists = Playlist.all 
+        render json: PlaylistSerializer.new(playlists)
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def show
+    end
+
+    def destroy
+    end
+
+    private
+        def playlist_params
+            params.require(:playlist).permit(:title, :description)
+        end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,29 @@
 class SongsController < ApplicationController
+    def index
+        songs = Song.all 
+        render json: SongSerializer.new(songs)
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def show
+    end
+
+    def destroy
+    end
+
+    private
+        def song_params
+            params.require(:song).permit(:title, :artist, :playlist_id)
+        end
 end
